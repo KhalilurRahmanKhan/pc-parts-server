37a9bf3f85f7cf3fc72c503ccc56e950310f28a7
KhalilurRahmanKhan <khanjahid373@gmail.com>
2022-05-28 18:09:07 +0600

    test commit

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000
 
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.p3yfj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -14,10 +15,19 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
+
+        const pcPartsDB = client.db("pcPartsDB");
+        const products = pcPartsDB.collection("products"); 
         
-        app.get('/', (req, res) => {
-            res.send('Hello World!')
+
+
+        app.post('/product/add', async (req, res) => {
+             const data = req.body;
+             const result = await products.insertOne(data);
+    
+             res.send(result);
           })
+
 
     } finally {
         await client.close();
