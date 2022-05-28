63cf38367c9f297ca25413a60db57f1899ca2842
KhalilurRahmanKhan <khanjahid373@gmail.com>
2022-05-28 23:43:07 +0600

    add product api

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const app = express()
+var cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express()
 
 const port = process.env.PORT || 5000
 
-
+app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.p3yfj.mongodb.net/?retryWrites=true&w=majority`;
@@ -14,13 +17,45 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        
+
+        const pcPartsDB = client.db("pcPartsDB");
+        const products = pcPartsDB.collection("products");
+
+
+        app.get('/product', async (req, res) => {
+            const query = req.query;
+            const cursor = await products.find(query).limit(3);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+        app.get('/product/:id', async (req, res) => {
+            let id = req.params.id;
+            const cursor =await products.find({_id:ObjectId(id)});
+            const result = await cursor.toArray();
+         res.send(result[0]);
+            });
+      
+
+
+
+        app.post('/product/add', async (req, res) => {
+            const data = req.body;
+            const result = await products.insertOne(data);
+            res.send(result);
+        });
+
+
+
+
+
+
+
         app.get('/', (req, res) => {
             res.send('Hello World!')
-          })
+        })
 
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 
